9e058e9ba3316acabafb0fa98de29518558e8f52
Jake Bell <jake.b.bell@gmail.com>
2010-12-12 21:16:40 -0600

    Adding colorbox plugin.

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,14 @@ module Ashint
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w(jquery jquery-ui jquery.dataTables rails)
+    config.action_view.javascript_expansions[:defaults] = 
+      %w(
+        jquery 
+        jquery-ui 
+        jquery.dataTables 
+        jquery.colorbox 
+        rails
+      )
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
