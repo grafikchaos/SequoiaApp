5d9882547499debe774f108a365d8dbe54eb3f22
Josh Johnson <josh@augustash.com>
2011-01-19 15:59:58 -0600

    added spec controllers and helpers for clients and bookmarks

---
+++ spec/controllers/clients_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe ClientsController, "create a new valid client" do
+  integrate_views
+  
+  it "should redirect to index with a notice on successful save" do
+    # TO DO
+  end
+end
