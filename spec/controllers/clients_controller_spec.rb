09b59bd673016e98c538f9111c60a2ba4b12a1aa
Jake Bell <jake@theunraveler.com>
2011-01-25 13:20:17 -0600

    - Removing rspec. - Regenerated models to test with Test::Unit - Added factory_girl stubs

--- spec/controllers/clients_controller_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe ClientsController, "create a new valid client" do
-  integrate_views
-  
-  it "should redirect to index with a notice on successful save" do
-    # TO DO
-  end
-end
