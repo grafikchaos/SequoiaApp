a3ab7d822630768d5355ab3f59b30395fc02d79b
Jake Bell <jake@theunraveler.com>
2011-02-03 14:39:17 -0600

    Adding required class to required fields.

--- db/schema.rb
+++ db/schema.rb
@@ -22,15 +22,15 @@ ActiveRecord::Schema.define(:version => 20110130020029) do
   add_index "bookmarks", ["user_id"], :name => "bookmarks_user_id_fk"
 
   create_table "clients", :force => true do |t|
-    t.string   "name",                            :default => "NULL", :null => false
-    t.string   "client_code",       :limit => 10, :default => "NULL", :null => false
+    t.string   "name",                            :default => "", :null => false
+    t.string   "client_code",       :limit => 10, :default => "", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "cached_slug"
     t.string   "logo_file_name"
     t.string   "logo_content_type"
     t.integer  "logo_file_size"
     t.datetime "logo_updated_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   add_index "clients", ["cached_slug"], :name => "index_clients_on_cached_slug"
@@ -86,14 +86,14 @@ ActiveRecord::Schema.define(:version => 20110130020029) do
   add_index "entity_types", ["name"], :name => "index_entity_types_on_name"
 
   create_table "projects", :force => true do |t|
-    t.integer  "client_id",                      :null => false
-    t.string   "name",       :default => "NULL", :null => false
+    t.integer  "client_id",  :default => 0,  :null => false
+    t.string   "name",       :default => "", :null => false
     t.string   "domain"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "projects", ["client_id"], :name => "projects_client_id_fk"
+  add_index "projects", ["client_id"], :name => "fk_projects_clients"
 
   create_table "slugs", :force => true do |t|
     t.string   "name"
@@ -135,6 +135,6 @@ ActiveRecord::Schema.define(:version => 20110130020029) do
   add_foreign_key "entity_rows", "entities", :name => "entity_rows_entity_id_fk", :dependent => :delete
   add_foreign_key "entity_rows", "entity_keys", :name => "entity_rows_entity_key_id_fk"
 
-  add_foreign_key "projects", "clients", :name => "projects_client_id_fk", :dependent => :delete
+  add_foreign_key "projects", "clients", :name => "fk_projects_clients", :dependent => :delete
 
 end
