7ab74fa8f58894b2749f554cf542078670cbf63a
Jake Bell <jake@theunraveler.com>
2011-02-15 00:58:42 -0600

    Passwords should now be hashed using base64.

--- app/helpers/entity_rows_helper.rb
+++ app/helpers/entity_rows_helper.rb
@@ -1,2 +1,10 @@
 module EntityRowsHelper
+
+  def decode_password(row)
+    if row.entity_key.name.downcase == 'password'
+      require "base64"
+      Base64.decode64(row.value)
+    end
+  end
+
 end
--- app/models/entity_row.rb
+++ app/models/entity_row.rb
@@ -2,4 +2,18 @@ class EntityRow < ActiveRecord::Base
   belongs_to :entity_key
   belongs_to :entity
   
+  before_save :encrypt_password
+
+  # Private methods below here!
+
+  private
+
+  def encrypt_password
+    ek = EntityKey.find(self.entity_key_id)
+    if ek.name.downcase == 'password'
+      require "base64"
+      encoded = Base64.encode64(self.value)
+      self.value = encoded
+    end
+  end
 end
