8e87eb3f108546fb3047bcc01ab89f6bcdfaf58f
Jake Bell <jake@theunraveler.com>
2011-02-15 01:00:24 -0600

    Oops. Problem with views helper.

--- app/helpers/entity_rows_helper.rb
+++ app/helpers/entity_rows_helper.rb
@@ -3,8 +3,9 @@ module EntityRowsHelper
   def decode_password(row)
     if row.entity_key.name.downcase == 'password'
       require "base64"
-      Base64.decode64(row.value)
+      password = Base64.decode64(row.value)
     end
+    password || row.value
   end
 
 end
