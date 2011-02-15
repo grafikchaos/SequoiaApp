532b8bb1d0b4d34a1300f8f270c2651e8d9065a1
Jake Bell <jake@theunraveler.com>
2011-02-15 09:23:04 -0600

    Forgot to include deleted files.

--- db/migrate/20110215070307_add_encryption_to_entity_keys.rb
+++
@@ -1,9 +0,0 @@
-class AddEncryptionToEntityKeys < ActiveRecord::Migration
-  def self.up
-    add_column :entity_keys, :encrypt, :boolean, :after => :name, :default => false
-  end
-
-  def self.down
-    remove_column :entity_keys, :encrypt
-  end
-end
