bb81ecae1b00d625e6c4765d757e41eb015fe6dd
Jake Bell <jake@theunraveler.com>
2011-02-23 19:26:32 -0600

    A start to the search model.

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,27 @@
 class Search
+
+  attr_accessor :query, :result
+
+  def initialize(query)
+    self.query = query
+    query.starts_with?('$') ? advanced : simple
+  end
+
+  ### PRIVATE METHODS BELOW ###
+  private
+
+  def simple
+    self.result = Client.simple_search(self.query)
+  end
+
+  def advanced
+    self.query.split(/\$/).each do |part|
+      split_types(part)
+    end
+  end
+
+  def split_types(part)
+      
+  end
+
 end
