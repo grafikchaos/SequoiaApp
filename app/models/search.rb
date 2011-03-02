6b89a8a57fd191ffa54bf744639915c09c639519
Jake Bell <jake@theunraveler.com>
2011-03-02 09:23:42 -0600

    Searching with entity types now works, albeit a bit janky.

--- app/models/search.rb
+++ app/models/search.rb
@@ -23,10 +23,35 @@ class Search
     # Remove the first $ to make our split cleaner.
     q = self.query.sub('$', '')
     q.split(/\$/).each do |clause|
-      split = clause.scan(/[^\s]+/)
-      client_code = split.first
-      row_value = split.values_at(1..split.count).join(' ').rstrip
-      self.result.concat(Entity.advanced_search(client_code, '', row_value))
+
+      # Divide the string on spaces.
+      words = clause.scan(/[^\s]+/)
+
+      # Client code will always be first, so we capture and remove it.
+      client_code = words.first
+      words.delete_at(0)
+      
+      # Put the rest back together.
+      string = words.join(' ').rstrip
+
+      if string.include? "#"
+        # Remove the first $ to make our split cleaner.
+        string.sub!('#', '')
+        string.split(/#/).each do |type_clause|
+
+          # Divide the string on spaces again.
+          words2 = type_clause.scan(/[^\s]+/)
+          type = words2.first
+          words2.delete_at(0)
+          puts "Type: #{type}, cc: #{client_code}"
+
+          # Put the rest back together.
+          string2 = words2.join(' ').rstrip
+          self.result.concat(Entity.advanced_search(client_code, type, string2))
+        end
+      else
+        self.result.concat(Entity.advanced_search(client_code, '', string))
+      end
     end
   end
 
