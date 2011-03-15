a192bdc163195368e509bc99eb4322cce867e736
Jake Bell <jake@theunraveler.com>
2011-03-15 18:28:20 -0500

    Adding flash message timeout.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,6 +4,7 @@
  */
 $(document).ready(function() {
   initCollapsibles();
+  initFlashHide();
   initSearch();
   initBookmarksMenu();
   initQtips();
@@ -33,58 +34,13 @@ var initCollapsibles = function() {
 };
 
 /**
- * Clear form data.
+ * Hide flash messages after a specified time.
  */
-var clearForm = function(element) {
-  $(element).find(':input').each(function() {
-    switch(this.type) {
-      case 'password':
-      case 'select-multiple':
-      case 'select-one':
-      case 'text':
-      case 'textarea':
-        $(this).val('');
-        break;
-      case 'checkbox':
-      case 'radio':
-        this.checked = false;
-        break;
-    }
-  });
-};
-
-/**
- * Generic loading message.
- */
-var startLoading = function(selector, message) {
-  
-  // Default message.
-  var message = message ? message : 'Loading';
-  
-  var html  = '<img src="/images/ajax-loader.gif" /> '
-            + message + '&hellip;';
-            
-  $(selector).html(html);
-};
-
-/**
- * Notices.
- */
-var postNotice = function(content, type) {
-  
-  var type = type ? type : 'notice';
-  
-  var html  = '<div class="flash-message '+type+'">'
-            + content
-            + '</div>';
-        
-  $('body').append(html);
-  $('.flash-message').fadeIn('slow', function() {
-    $(this).delay(1500).fadeOut('slow', function() {
-      $(this).remove();
-    });
-  });
-};
+var initFlashHide = function() {
+  if ($('#flash-messages').length > 0) {
+    $('#flash-messages').delay(1000).slideUp();
+  }
+}
 
 /**
  * Client page search form.
