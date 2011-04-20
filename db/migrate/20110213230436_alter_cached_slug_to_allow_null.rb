c2afae6afe22b02c7cea15faabfee483403f7f2a
grafikchaos <josh@grafikchaos.com>
2011-04-20 11:23:43 -0500

    removing the reference to entities.cached_slug from the migration file

--- db/migrate/20110213230436_alter_cached_slug_to_allow_null.rb
+++ db/migrate/20110213230436_alter_cached_slug_to_allow_null.rb
@@ -1,13 +1,11 @@
 class AlterCachedSlugToAllowNull < ActiveRecord::Migration
   def self.up
     change_column(:entity_types, :cached_slug, :string, :null => true)
-    change_column(:entities, :cached_slug, :string, :null => true)
     change_column(:entity_keys, :cached_slug, :string, :null => true)
   end
 
   def self.down
     change_column(:entity_types, :cached_slug, :string, :null => false)
-    change_column(:entities, :cached_slug, :string, :null => false)
     change_column(:entity_keys, :cached_slug, :string, :null => false)
   end
 end
